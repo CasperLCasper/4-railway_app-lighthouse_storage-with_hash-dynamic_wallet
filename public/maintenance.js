67a642662a3479509bd346dad483915088465b1d
CasperLCasper <64712311+CasperLCasper@users.noreply.github.com>
2026-06-09 01:22:57 +0300

    Update maintenance.js

--- public/maintenance.js
+++ public/maintenance.js
@@ -4,7 +4,7 @@
 
 export const MAINTENANCE_CONFIG = {
   // 🛠️ NOMAINI UZ true, LAI IESLĒGTU APKOPES REŽĪMU // NOMAINI UZ false, LAI IZSLĒGTU APKOPES REŽĪMU
-  isMaintenance: true, 
+  isMaintenance: false, 
   
   // Teksti, kas parādīsies uz Canvas ekrāna
   title: '🛠️ SYSTEM UNDER MAINTENANCE 🛠️',
